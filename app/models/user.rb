404a78c18a80d35daa5fa6bf31224d7504bd041b
Jose <escobedo.jose48@outlook.com>
2022-06-23 16:07:55 -0700

    adding email validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, presence: true, uniqueness: true
     validates :password, length: {minimum: 6, allow_nil: true}
+    validates :email, presence: true, uniqueness: true
 
 end
